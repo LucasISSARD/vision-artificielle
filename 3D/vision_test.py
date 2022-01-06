53d2c7e9fae0bca6e18dbe3299a7c8c174ed7f1b
Quentbern <79104974+Quentbern@users.noreply.github.com>
2022-01-06 09:56:56 +0100

    Update vision_test.py

--- 3D/vision_test.py
+++ 3D/vision_test.py
@@ -10,14 +10,21 @@ import cv2
 from matplotlib import pyplot as plt
 
 
-
+# Charger les photos
 img_filename_1 = 'C:/Users/quent/Desktop/Cours 5A/VISION_PROJET/data_scene_flow/testing/image_2/000000_10.png'  # Load the photo
 img_filename_2 = 'C:/Users/quent/Desktop/Cours 5A/VISION_PROJET/data_scene_flow/testing/image_3/000000_10.png'  # Load the photo
-# Plot the Photo
-fig=plt.figure(figsize=(8,8))
-img_i = cv2.imread(img_filename)
 
-
+# Photo 1
+fig=plt.figure(figsize=(6,6))
+img_i_1 = cv2.imread(img_filename_1)
 plt.axis('off')
-plt.imshow(cv2.cvtColor(img_i, cv2.COLOR_BGR2RGB))
+plt.imshow(cv2.cvtColor(img_i_1, cv2.COLOR_BGR2RGB))
+
+# Photo 2
+fig=plt.figure(figsize=(6,6))
+img_i_2 = cv2.imread(img_filename_2)
+plt.axis('off')
+plt.imshow(cv2.cvtColor(img_i_2, cv2.COLOR_BGR2RGB))
+
+
 plt.show()
