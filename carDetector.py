9ee118fe60811c251abdf02839a52806c6a36468
LucasISSARD <lucas.issard@orange.fr>
2022-01-13 10:38:43 +0100

    Updated carDetector.py
    Fonctionne avec une vidéo mais la détection est un peu erratique

--- carDetector.py
+++ carDetector.py
@@ -13,13 +13,13 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Chemin de la vidéo
-video_path = "D:/Documents/GitHub/vision-artificielle/dataset/2011_09_26/2011_09_26_drive_0048_sync/image_02/data/"
+# Chemin de la vidéo ( /!\ sur Windows, remplacer les \ par des / sans oublier le / final)
+video_path = "D:/Documents/GitHub/vision-artificielle/dataset/Road/"
 
 # Import du classifieur pré-entraîné pour la détection de voitures
 car_cascade = cv2.CascadeClassifier('haarcascade_car.xml') 
 
-counter = 0
+counter = 70        # Première image
 files = next(os.walk(video_path))[2]    # Compte le nombre de fichiers dans le video_path
 while counter < len(files):             # Pour toutes les images du video_path
     # Acquisition des images dans la vidéo
@@ -31,17 +31,18 @@ while counter < len(files):             # Pour toutes les images du video_path
     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Affichage de l'image
-    cv2.imshow("video", img)
-    counter = counter + 1
-    cv2.waitKey(1)
-
     # Détection des voitures dans notre image
-    cars = car_cascade.detectMultiScale(img_gray, scaleFactor = 1.03, minNeighbors = 6)
+    cars = car_cascade.detectMultiScale(img_gray, scaleFactor = 1.08, minNeighbors = 4)
 
     # Dessin des bounding boxes
     for (x,y,w,h) in cars:
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+
+    # Affichage de l'image
+    cv2.imshow("video", img)
+    counter = counter + 1
+    cv2.waitKey(1)
+    time.sleep(0.1)
 
 
 cv2.destroyAllWindows()
