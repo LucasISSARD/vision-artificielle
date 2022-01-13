cb552c3c833e555d997feebe3090b2ac20c0c2b6
Zintory <97219724+Zintory@users.noreply.github.com>
2022-01-13 11:38:19 +0100

    Debuggage

--- 3D/Fonctions_sup.py
+++ 3D/Fonctions_sup.py
@@ -4,6 +4,7 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 from math import *
+import scipy as sc
 
 
 
@@ -11,8 +12,8 @@ def correlation_2D(I1,I2):
 
 
 
-    I1_int=I1.astype(float)
-    I2_int=I2.astype(float)
+    I1_int=I1#.astype(float)
+    I2_int=I2#.astype(float)
 
     m1=np.mean(I1_int)
     A=np.sum((I1_int-m1)**2)
@@ -24,7 +25,7 @@ def correlation_2D(I1,I2):
     if B == 0:
         rep = 0
     else :
-        rep = np.sum(np.sum(I2_int-m2)*(I1_int-m1))/sqrt(A*B)
+        rep = np.sum(np.sum((I2_int-m2)*(I1_int-m1)))/sqrt(A*B)
 
 
     return (rep)
