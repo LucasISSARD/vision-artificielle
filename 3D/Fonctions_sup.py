c77820341799af18dcc0caa7b423f1c924c4961e
Zintory <97219724+Zintory@users.noreply.github.com>
2022-01-18 01:54:52 +0100

    Fonction triangulation
    rajout

--- 3D/Fonctions_sup.py
+++ 3D/Fonctions_sup.py
@@ -30,7 +30,81 @@ def correlation_2D(I1,I2):
 
     return (rep)
 
+def calc_d_cam_to_cam(T2,T3):
+    d_X=T2[0]+T3[0]
+    d_Z=T2[2]+T3[2]
+
+    d_cam=sqrt(d_X**2+d_Z**2)
+
+    return(d_cam)
 
 
+def triangulation (dist_pix_im_g,dist_pix_im_d,L_ecrant, angle_d_ouverture,AB):
 
+    pi=np.pi
+
+    L_ecrant_moitier=L_ecrant/2
+
+    d_ecrant=L_ecrant_moitier/np.tan(angle_d_ouverture/2)
+
+    alpha_prime= np.arctan(abs(dist_pix_im_g-L_ecrant_moitier)/d_ecrant)
+    beta_prime = np.arctan(abs(dist_pix_im_d-L_ecrant_moitier)/d_ecrant)
+
+
+    if dist_pix_im_g >=L_ecrant_moitier :
+        alpha= pi/2-alpha_prime
+    else :
+        alpha= pi/2+alpha_prime
+
+    if dist_pix_im_d >=L_ecrant_moitier :
+        beta = pi/2+ beta_prime
+    else :
+        beta = pi/2- beta_prime
+
+    gama = pi-alpha-beta
+
+    # print(degrees(alpha))
+    # print(degrees(beta))
+    # print(degrees(gama))
+
+    BC=AB*np.sin(alpha)/np.sin(gama)
+
+    # print(AB)
+    # print(BC)
+
+    distance=np.sin(beta)*BC
+
+    return(distance)
+
+## test correlation 2D
+# a=[[1,2,3],[4,5,6]]
+# b=a
+#
+# m=np.mean(a)
+# n=np.mean(b)
+# A=np.sum((a-m)**2)
+# B=np.sum((b-n)**2)
+# rep = np.sum(np.sum((b-n)*(a-m)))/sqrt(A*B)
+#
+# rep_2=correlation_2D(a,b)
+
+## test triangulation
+
+# T2= [5.956621e-02, 2.900141e-04, 2.577209e-03]
+# T3= [-4.731050e-01, 5.551470e-03,-5.250882e-03]
+# AB= calc_d_cam_to_cam(T2,T3)
+#
+# angle_d_ouverture=np.pi/2
+#
+# dist_pix_im_g=270
+# dist_pix_im_d=211
+# L_ecrant=1242
+#
+#
+# rep=triangulation (dist_pix_im_g,dist_pix_im_d,L_ecrant, angle_d_ouverture,AB)
+#
+# print(rep)
+
+
+## autre
 #verification peut etre pas besion de R_rect
